f41eb4c0d0c4c24702a859243594b7dd9ed1df98
abhishekY2401 <abhiyd2401@gmail.com>
2021-10-21 17:13:13 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Weather App</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
