356ecb4d02e8c8ad71fb7c686fbe833f57908ff3
abhishekY2401 <abhiyd2401@gmail.com>
2021-10-28 20:42:11 +0530

    made changes by displaying current location via api calling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  // show location, day date and time,  weather in degrees celsius, type of weather, parameters of weather
+  const apiKey = "85a0f9c6e34c4e619b5124543212610";
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          if (data.code === 1006) {
+            setWeatherData(data.message);
+          }
+        });
+      setCity("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Weather App</h2>
-      </header>
+    <div className="container">
+      <main>
+        <input
+          type="search"
+          placeholder="Search by city"
+          onChange={(e) => setCity(e.target.value)}
+          value={city}
+          onKeyPress={getWeather}
+        />
+      </main>
+      <div className="weather">
+        <i className="fas fa-map-marker-alt"></i>
+        <p className="location">
+          {weatherData && weatherData.location && weatherData.location.name},{" "}
+          {weatherData && weatherData.location && weatherData.location.country}
+        </p>
+      </div>
     </div>
   );
 }
