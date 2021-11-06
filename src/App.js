1922e357c0bcb910a085e650bc76553a07150ebc
abhishekY2401 <abhiyd2401@gmail.com>
2021-11-06 14:52:02 +0530

    Added images

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React, { useState } from "react";
-import winter from "./assets/winter-bg.jpg";
 
 function App() {
   // show location, day date and time,  weather in degrees celsius, type of weather, parameters of weather
-  const apiKey = "85a0f9c6e34c4e619b5124543212610";
+  const apiKeyWeather = "85a0f9c6e34c4e619b5124543212610";
+  const apiKeyImage = "FUvogg6bXQSTCVHO4w48gWpHVLveHYbiUG8RLIHrLUs";
   const [weatherData, setWeatherData] = useState([{}]);
+  const [image, setImage] = useState("");
   const [city, setCity] = useState("");
 
   const getWeather = (event) => {
     if (event.key === "Enter") {
-      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKeyWeather}&q=${city}`)
         .then((response) => response.json())
         .then((data) => {
           setWeatherData(data);
-          if (data.code === 1006) {
-            setWeatherData(data.message);
-          }
+          getImage();
         });
       setCity("");
     }
   };
+
+  const getImage = () => {
+    fetch(`https://api.unsplash.com/search/photos/?query=${city}&client_id=${apiKeyImage}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setImage(data);
+    });
+  }
+
   return (
     <div className="container">
+      <div className="bg-img">
+        <img 
+        src={
+          image && 
+          image.results[1] && 
+          image.results[1].links && 
+          image.results[1].links.download
+        } 
+          className="bg-img"
+          alt="bg"/>
+      </div>
       <main>
         <input
           type="search"
