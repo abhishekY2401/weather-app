b13bef43b3570b4fc5f7ca60e13e39120e2c8cc1
abhishekY2401 <abhiyd2401@gmail.com>
2022-01-07 17:57:10 -0800

    made a 404Page Component

---
+++ src/Components/Error404.js
@@ -0,0 +1,11 @@
+
+const Error404 = () => {
+
+    return (
+        <h1 className="err">No Matching Location Found!<br/>
+        <span>Please try to search for a different city</span>
+        </h1>
+    )
+}
+
+export default Error404;
