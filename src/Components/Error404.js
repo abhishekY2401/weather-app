2b1199a1f867befb172952852f9ba027ba009a80
abhishekY2401 <abhiyd2401@gmail.com>
2022-01-16 16:54:20 +0530

    changed the className

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -2,8 +2,8 @@
 const Error404 = () => {
 
     return (
-        <h1 className="err">No Matching Location Found!<br/>
-        <span>Please try to search for a different city</span>
+        <h1 className="no-match">No Matching Location Found!<br/>
+           <span>Please try to search for a different city</span>
         </h1>
     )
 }
